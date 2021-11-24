90094d241b24bf57ec360816b067a1defbccc496
jacopo35 <jaczor00@gmail.com>
2021-11-24 15:33:09 +0100

    add-buttonjs & html

--- mail/js/script.js
+++ mail/js/script.js
@@ -6,13 +6,17 @@ stampa un messaggio appropriato sull’esito del controllo.
 
 
 
-const mail = prompt("dammi la tua mail");
+// const mail = prompt("Digita la tua mail");
+const input = document.querySelector("input");
+const container = document.querySelector(".container");
 
 const mails = ["jaczor00@gmail.com","thomas-23@teacher.it", "elisabetta@-24teacher.it"]
 
+const button = document.querySelector("button");
+
 let cond = false;
 
-for (let i = 0; i < mails.length; i++) {
+/*for (let i = 0; i < mails.length; i++) {
     if (mails[i] == mail) {
         cond = true;
     }
@@ -23,3 +27,18 @@ if (cond == true) {
 } else {
     console.log("non puoi accedere");
 }
+*/
+
+button.addEventListener("click", function () {
+    for (let i = 0; i < mails.length; i++) {
+        if (mails[i] == input.value) {
+            cond = true;
+        }
+    }
+
+    if (cond == true) {
+        container.innerHTML = "<h1> Puoi accedere! :) </h1>"
+    } else {
+        container.innerHTML = "<h1> Non puoi accedere :( </h1>"
+    }
+})
