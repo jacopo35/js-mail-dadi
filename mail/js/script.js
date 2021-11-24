e3e230c404f7d5943571db0cdfc9ac812d2b49de
jacopo35 <jaczor00@gmail.com>
2021-11-24 15:26:49 +0100

    add-js-basic-command

--- mail/js/script.js
+++ mail/js/script.js
@@ -4,3 +4,22 @@ controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
 */
 
+
+
+const mail = prompt("dammi la tua mail");
+
+const mails = ["jaczor00@gmail.com","thomas-23@teacher.it", "elisabetta@-24teacher.it"]
+
+let cond = false;
+
+for (let i = 0; i < mails.length; i++) {
+    if (mails[i] == mail) {
+        cond = true;
+    }
+}
+
+if (cond == true) {
+    console.log("puoi accedere");
+} else {
+    console.log("non puoi accedere");
+}
