49e19c5df976b056806c7a59d6090bc6beb87797
jacopo35 <jaczor00@gmail.com>
2021-11-24 15:55:15 +0100

    add-const

--- mail/js/script.js
+++ mail/js/script.js
@@ -8,25 +8,17 @@ stampa un messaggio appropriato sull’esito del controllo.
 
 // const mail = prompt("Digita la tua mail");
 const input = document.querySelector("input");
+
 const container = document.querySelector(".container");
-
 const mails = ["jaczor00@gmail.com","thomas-23@teacher.it", "elisabetta@-24teacher.it"]
-
 const button = document.querySelector("button");
-
+const remove = document.querySelector(".remove")
 let cond = false;
 
-/*for (let i = 0; i < mails.length; i++) {
-    if (mails[i] == mail) {
-        cond = true;
-    }
-}
-
-if (cond == true) {
-    console.log("puoi accedere");
-} else {
-    console.log("non puoi accedere");
-}
+/*remove.addEventListener("click", function () {
+    container.innerHTML = "";
+    input.value = "";
+})
 */
 
 button.addEventListener("click", function () {
@@ -37,7 +29,7 @@ button.addEventListener("click", function () {
     }
 
     if (cond == true) {
-        container.innerHTML = "<h1> Puoi accedere! :) </h1>"
+        container.innerHTML = "<h1> Ora puoi accedere :) </h1>"
     } else {
         container.innerHTML = "<h1> Non puoi accedere :( </h1>"
     }
